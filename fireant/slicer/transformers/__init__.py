06f62510d72ac5a00e0b72d111ce800dcde0a576
Timothy Heys <theys@kayak.com>
2016-08-10 18:05:14 +0200

    Removed an import that should not yet be included

--- fireant/slicer/transformers/__init__.py
+++ fireant/slicer/transformers/__init__.py
@@ -3,4 +3,3 @@
 from .base import Transformer, TransformationException
 from .datatables import TableIndex, DataTablesTransformer, CSVTransformer
 from .highcharts import HighchartsTransformer, HighchartsColumnTransformer
-from .notebook import MatplotlibTransformer, PandasTransformer
