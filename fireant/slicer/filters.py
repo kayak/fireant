4481bb5ab384df1c0c44b2e827c21c0296774901
Timothy Heys <theys@kayak.com>
2016-08-24 19:22:01 +0200

    Added tests for filters

--- fireant/slicer/filters.py
+++ fireant/slicer/filters.py
@@ -16,15 +16,6 @@ class EqualityFilter(Filter):
         self.operator = operator
         self.value = value
 
-    def __eq__(self, other):
-        if not isinstance(other, EqualityFilter):
-            return False
-
-        return self.element_key == other.element_key and self.operator == other.operator and self.value == other.value
-
-    def __hash__(self):
-        return hash('%s_%s_%s' % (self.element_key, self.operator, self.value))
-
     def schemas(self, element):
         element = wrap_list(element)
         value = wrap_list(self.value)
@@ -45,15 +36,6 @@ class ContainsFilter(Filter):
         super(ContainsFilter, self).__init__(element_key)
         self.values = values
 
-    def __eq__(self, other):
-        if not isinstance(other, ContainsFilter):
-            return False
-
-        return self.element_key == other.element_key and self.values == other.values
-
-    def __hash__(self):
-        return hash('%s_%s_%s' % (self.__class__, self.element_key, self.values))
-
     def schemas(self, element):
         return element.isin(self.values)
 
@@ -63,15 +45,6 @@ class RangeFilter(Filter):
         super(RangeFilter, self).__init__(element_key)
         self.start = start
         self.stop = stop
-
-    def __eq__(self, other):
-        if not isinstance(other, RangeFilter):
-            return False
-
-        return self.element_key == other.element_key and self.start == other.start and self.stop == other.stop
-
-    def __hash__(self):
-        return hash('%s_%s_%s_%s' % (self.__class__, self.element_key, self.start, self.stop))
 
     def schemas(self, element):
         element = wrap_list(element)
@@ -94,14 +67,5 @@ class WildcardFilter(Filter):
         super(WildcardFilter, self).__init__(element_key)
         self.value = value
 
-    def __eq__(self, other):
-        if not isinstance(other, EqualityFilter):
-            return False
-
-        return self.element_key == other.element_key and self.value == other.value
-
     def schemas(self, element):
         return element.like(self.value)
-
-    def __hash__(self):
-        return hash('%s_%s_%s' % (self.__class__, self.element_key, self.value))
