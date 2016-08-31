ad675127c4555bd2e98bc6da6ea6c17366ca1fa5
Timothy Heys <theys@kayak.com>
2016-08-31 14:17:58 +0200

    Added stubs for L1 and L2 loss ops

--- fireant/slicer/operations.py
+++ fireant/slicer/operations.py
@@ -5,12 +5,29 @@ class Operation(object):
     """
     The `Operation` class represents an operation in the `Slicer` object.
     """
-
-    def __init__(self, key):
-        self.key = key
+    pass
 
 
 class Totals(Operation):
+    """
+    Operation for rolling up totals across dimensions in queries.
+    """
+    key = 'totals'
+
     def __init__(self, *dimension_keys):
-        super(Totals, self).__init__('totals')
         self.dimension_keys = dimension_keys
+
+
+class L1Loss(Operation):
+    """
+    Converts the result of one or more metrics into an l1
+    """
+    key = 'l1_loss'
+
+    def __init__(self, metric_key, error_key):
+        self.metric_key = metric_key
+        self.error_key = error_key
+
+
+class L2Loss(L1Loss):
+    key = 'l2_loss'
