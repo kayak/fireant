f2409222bbd0329784fc50e9335d5b12a9d13e04
Michael England <mengland@kayak.com>
2016-12-22 17:50:50 +0100

    Added fix for operations not appearing when using widget groups

--- fireant/dashboards/managers.py
+++ fireant/dashboards/managers.py
@@ -2,6 +2,7 @@
 import pandas as pd
 
 from fireant import utils
+from fireant.slicer.managers import Totals
 
 
 class WidgetGroupManager(object):
@@ -25,24 +26,31 @@ class WidgetGroupManager(object):
         )
 
         return list(self._transform_widgets(self.widget_group.widgets, dataframe,
-                                            combined_dimensions, combined_references))
+                                            combined_dimensions, combined_references, combined_operations))
 
-    def _transform_widgets(self, widgets, dataframe, dimensions, references):
+    def _transform_widgets(self, widgets, dataframe, dimensions, references, operations):
         for widget in widgets:
             display_schema = self.widget_group.slicer.manager.display_schema(
                 metrics=widget.metrics,
                 dimensions=dimensions,
                 references=references,
+                operations=operations,
             )
+
+            # Temporary fix to enable operations to get output properly. Can removed when the Fireant API is refactored.
+            operation_columns = ['{}_{}'.format(operation.metric_key, operation.key)
+                                 for operation in operations if operation.key != Totals.key]
+
+            columns = utils.flatten(widget.metrics) + operation_columns
 
             if references:
                 # This escapes a pandas bug where a data frame subset of columns still returns the columns of the
                 # original data frame
                 reference_keys = [''] + [ref.key for ref in references]
-                subset_columns = pd.MultiIndex.from_product([reference_keys, utils.flatten(widget.metrics)])
+                subset_columns = pd.MultiIndex.from_product([reference_keys, columns])
                 subset = pd.DataFrame(dataframe[subset_columns], columns=subset_columns)
 
             else:
-                subset = dataframe[utils.flatten(widget.metrics)]
+                subset = dataframe[columns]
 
             yield widget.transformer.transform(subset, display_schema)
