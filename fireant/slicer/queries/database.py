73c1b21c802a3214973b8e782b22afe9fd9ec331
Michael England <mengland@kayak.com>
2018-03-08 15:56:13 +0100

    Added slow query warning log for queries over 15s

--- fireant/slicer/queries/database.py
+++ fireant/slicer/queries/database.py
@@ -1,10 +1,14 @@
 import time
-from typing import Iterable
 
 import pandas as pd
+from typing import Iterable
 
 from fireant.database.base import Database
-from .logger import logger
+from .logger import (
+    SLOW_QUERY_LOG_MIN_DURATION,
+    query_logger,
+    slow_query_logger,
+)
 from ..dimensions import (
     ContinuousDimension,
     Dimension,
@@ -26,13 +30,16 @@ def fetch_data(database: Database, query: str, dimensions: Iterable[Dimension]):
     :return: `pd.DataFrame` constructed from the result of the query
     """
     start_time = time.time()
-    logger.debug(query)
+    query_logger.debug(query)
 
     data_frame = database.fetch_data(str(query))
 
-    logger.info('[{duration} seconds]: {query}'
-                .format(duration=round(time.time() - start_time, 4),
-                        query=query))
+    duration = round(time.time() - start_time, 4)
+    query_log_msg = '[{duration} seconds]: {query}'.format(duration=duration, query=query)
+    query_logger.info(query_log_msg)
+
+    if duration >= SLOW_QUERY_LOG_MIN_DURATION:
+        slow_query_logger.warning(query_log_msg)
 
     return clean_and_apply_index(data_frame, dimensions)
 
--- fireant/slicer/queries/logger.py
+++ fireant/slicer/queries/logger.py
@@ -1,3 +1,7 @@
 import logging
 
-logger = logging.getLogger('fireant.query_log$')
+SLOW_QUERY_LOG_MIN_DURATION = 15
+
+query_logger = logging.getLogger('fireant.query_log')
+
+slow_query_logger = logging.getLogger('fireant.slow_query_log')
