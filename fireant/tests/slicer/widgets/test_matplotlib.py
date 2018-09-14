5ff8aa8378a9e9685d5272c21befb1fff3010cec
Timothy Heys <theys@kayak.com>
2018-09-14 09:41:46 +0200

    Removing matplotlib from travis-ci yaml and making it possible to skip matplotlib tests if matplotlib is not installed

--- fireant/tests/slicer/widgets/test_matplotlib.py
+++ fireant/tests/slicer/widgets/test_matplotlib.py
@@ -6,17 +6,24 @@ from fireant.tests.slicer.mocks import (
     slicer,
 )
 
+try:
+    # travis-ci cannot run these tests because it won't install matplotlib
+    import matplotlib
 
-class MatplotlibLineChartTransformerTests(TestCase):
-    maxDiff = None
 
-    chart_class = Matplotlib.LineSeries
-    chart_type = 'line'
-    stacking = None
+    class MatplotlibLineChartTransformerTests(TestCase):
+        maxDiff = None
 
-    def test_single_metric_line_chart(self):
-        result = Matplotlib(title="Time Series, Single Metric") \
-            .axis(self.chart_class(slicer.metrics.votes)) \
-            .transform(cont_dim_df, slicer, [slicer.dimensions.timestamp], [])
+        chart_class = Matplotlib.LineSeries
+        chart_type = 'line'
+        stacking = None
 
-        self.assertEqual(1, len(result))
+        def test_single_metric_line_chart(self):
+            result = Matplotlib(title="Time Series, Single Metric") \
+                .axis(self.chart_class(slicer.metrics.votes)) \
+                .transform(cont_dim_df, slicer, [slicer.dimensions.timestamp], [])
+
+            self.assertEqual(1, len(result))
+
+except ImportError:
+    pass
