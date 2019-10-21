2cf89e5548703213ce0603c43f7df02f1cf405b4
Timothy Heys <theys@kayak.com>
2019-10-21 15:55:35 +0200

    fixed snowflake dep in setup.py

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ setup(
       ],
       extras_require={
           'vertica': ['vertica-python==0.7.3'],
-          'snowflake': ['snowflake-connector-python==1.7.2',
+          'snowflake': ['snowflake-connector-python==1.9.1',
                         'cryptography==2.4.2'],
           'mysql': ['pymysql==0.8.0'],
           'redshift': ['psycopg2==2.7.3.2'],
