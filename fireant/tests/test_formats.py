f81365c7464eafd06758d92ebc8e07aafed62620
Michael England <mengland@kayak.com>
2020-09-10 09:03:24 +0200

    Ensure precision is retained after converting percentage to decimal

--- fireant/tests/test_formats.py
+++ fireant/tests/test_formats.py
@@ -262,5 +262,10 @@ class FormatDisplayValueStyleTests(TestCase):
         self.assertEqual("1244996.1138", formats.display_value(1244996.1138000000000000, field, use_raw_value=True))
 
     def test_converts_percentage_to_decimal_when_use_raw_value_True_and_suffix_is_percentage(self):
-        field = Field("number", None, data_type=DataType.number, suffix="%")
-        self.assertEqual("0.87123123131", formats.display_value(87.123123131, field, use_raw_value=True))
+        with self.subTest('when no precision'):
+            field = Field("number", None, data_type=DataType.number, suffix="%")
+            self.assertEqual("0.87123123131", formats.display_value(87.123123131, field, use_raw_value=True))
+
+        with self.subTest('when precision'):
+            field = Field("number", None, data_type=DataType.number, suffix="%", precision=2)
+            self.assertEqual("0.0739", formats.display_value(7.38652, field, use_raw_value=True))
